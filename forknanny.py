106e8a95440d499add7396003f7492089eb6aa5f
Jakub Wilk <jwilk@jwilk.net>
2019-02-04 09:17:39 +0100

    Initial import.

---
+++ forknanny.py
@@ -0,0 +1,33 @@
+# encoding=UTF-8
+
+# Copyright © 2019 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+'''
+forbid os.fork() in multi-threaded programs
+'''
+
+import os
+import errno
+import threading
+
+_orig_fork = os.fork
+
+def fork():
+    threads = threading.enumerate()
+    if len(threads) > 1:
+        err = errno.EDEADLK
+        msg = (
+            os.strerror(err) + '\n'
+            'Active threads:\n'
+            + '\n'.join('* ' + repr(t) for t in threads)
+        )
+        raise OSError(err, msg)
+    return _orig_fork()
+
+def install():
+    os.fork = fork
+
+__all__ = ['install']
+
+# vim:ts=4 sts=4 sw=4 et
---
+++ test.py
@@ -0,0 +1,41 @@
+# encoding=UTF-8
+
+# Copyright © 2019 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+import os
+import threading
+import time
+import unittest
+
+import forknanny
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        forknanny.install()
+
+    def test(self):
+        if os.fork() == 0:
+            os._exit(0x37)
+        (pid, status) = os.wait()
+        self.assertEqual(status, 0x3700)
+        evt = threading.Event()
+        thr = threading.Thread(target=evt.wait)
+        thr.start()
+        with self.assertRaises(OSError):
+            if os.fork() == 0:
+                os._exit(0x42)
+            (pid, status) = os.wait()
+            self.assertEqual(status, 0x4200)
+        evt.set()
+        thr.join()
+        if os.fork() == 0:
+            os._exit(0x42)
+        (pid, status) = os.wait()
+        self.assertEqual(status, 0x4200)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vim:ts=4 sts=4 sw=4 et
